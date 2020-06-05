0ff31eed59b071549ed1362240e899f7bbd21479
Reuben Ninan <intelphysic@gmail.com>
2020-06-05 19:52:20 -0400

    nextjs init

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+
+function Home(){
+  
+  return (
+    <div>
+      <Head>
+        <title>VennFX</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      </Head>
+      
+      <span class="bg-error text-white">VennFX</span>
+      
+
+    </div>
+  );
+
+}
+
+export default Home;
